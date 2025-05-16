36aa7445e2f53c4009addb8cadcc23e0cbf04056
Denis <denik.savic2007@gmail.com>
2025-05-16 20:34:12 +0300

    added lab 10
    мой семён мой коллега семён

---
+++ Лабы/Лабораторная работа 10/SE_Lab10/dop2/dop1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	auto lamb = [](int a)->bool {
+		return a > 0;
+		};
+	cout << (lamb(-5)?"true":"false") << ' ' << (lamb(3)?"true":"false");
+	return 0;
+}
